a574b2fe9590870f154e5374d174d8b85ecf608c
Edward Tanguay (windows) <edward@tanguay.info>
2023-07-12 14:47:04 +0200

    loading and showing number

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
+
+const url = 'http://localhost:4801/frameworks';
 
 function App() {
+	const [frameworks, setFrameworks] = useState([]);
 
-  return (
-    <div>
-      <h1>Frameworks</h1>
-      testing
-    </div>
-  )
+	useEffect(() => {
+		setTimeout(async () => {
+			const response = await axios.get(url);
+			const _frameworks = response.data;
+			setFrameworks(_frameworks);
+		}, 1500);
+	}, []);
+
+	return (
+		<div>
+			<h1>Frameworks</h1>
+			{frameworks.length > 0 ? (
+				<p>There are {frameworks.length} frameworks.</p>
+			) : (
+				<div>loading...</div>
+			)}
+		</div>
+	);
 }
 
-export default App
+export default App;
